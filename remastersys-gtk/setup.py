88d0107343688e2abca2d1c81d6ec005ea7b7990
CallumAuger <CallumAuger@users.noreply.github.com>
2014-05-31 11:30:50 +0100

    Update setup.py

--- remastersys-gtk/setup.py
+++ remastersys-gtk/setup.py
@@ -4,16 +4,16 @@
 import glob
 from setuptools import setup, find_packages
 
-setup(name = 'remastersys-gtk',
-    version = '3.0.4',
-    description='Ubuntu and variant system remaster. This is the alternate gtk gui for remastersys written in pygtk.',
+setup(name = 'linuxcreator-gtk',
+    version = '1.0.0',
+    description='Ubuntu and variant system remaster. This is the alternate gtk gui for linuxcreator written in pygtk.',
     author='Tony Brijeski',
     author_email='tb6517@yahoo.com',
     url='http://www.remastersys.com',
-    scripts=['remastersys-gtk'],
+    scripts=['linuxcreator-gtk'],
     packages = find_packages(),
     data_files = [
-        ('share/remastersys-gtk/ui/', glob.glob('data/ui/*.glade')),
+        ('share/linuxcreator-gtk/ui/', glob.glob('data/ui/*.glade')),
         ('share/remastersys-gtk/pixmaps/', glob.glob('data/pixmaps/*.png')),
         ('bin/', glob.glob('data/scripts/plymouth-preview')),
         ('share/man/man1/', glob.glob('man/man1/*.1')),
